7f9da210e70718051bd0c50d8eca39347a7a1e9e
upendra9 <upendra9@users.noreply.github.com>
2022-01-30 01:41:54 +0530

    Update filename.ts

--- typescript/filename.ts
+++ typescript/filename.ts
@@ -1,7 +1,7 @@
 //https://www.educative.io/blog/typescript-tutorial
 //https://www.javatpoint.com/typescript-vs-es6
 //https://www.greycampus.com/blog/programming/java-script-versions
-//
+//https://www.typescriptlang.org/play
 
 //TYpescript = ES6 + some additional functionalities
 //Typescript - transpiler, static typing , has interfaces , supports optional parameters
